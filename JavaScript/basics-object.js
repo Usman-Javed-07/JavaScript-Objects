df913c8d7f9a4bec76c996fbac5c7ea01df25e80
Usman Javed <usmanjaved0816@gmail.com>
2024-10-01 13:18:00 +0500

    basics of objects in js

---
+++ JavaScript/basics-object.js
@@ -0,0 +1,16 @@
+//   object
+
+const circle = {
+  radius : 1,
+  location :{
+    x : 1,
+    y : 1
+  },
+  isvisible : true,
+  draw: function() {
+    console.log("draw function");
+  }
+
+};
+
+circle .draw();    //  method
