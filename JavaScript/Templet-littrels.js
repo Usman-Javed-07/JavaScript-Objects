365ad47ff10fbb2e4a11848ac239a09ef6fe8b37
Usman Javed <usmanjaved0816@gmail.com>
2024-10-01 19:01:34 +0500

    templet literals

---
+++ JavaScript/Templet-littrels.js
@@ -0,0 +1,13 @@
+//    templet literals 
+
+let Name = "john";
+const message = ` hi ${Name}
+
+ how are you doing 
+
+ thank you for joining us 
+
+ regards 
+ Usman
+`
+console.log (message);
---
+++ JavaScript/string.js
@@ -0,0 +1,14 @@
+
+
+//  string 
+
+
+// string premitive
+const message = 'this is a string';
+
+
+
+
+// string object
+
+const another = new String ('hi');
