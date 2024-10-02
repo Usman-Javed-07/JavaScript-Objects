bbe99095006e95dbae74a86d94b1c9878ad83b87
Usman Javed <usmanjaved0816@gmail.com>
2024-10-02 13:26:30 +0500

    Exercise 4 solve

---
+++ JavaScript/exercise-4-blog-post-object.js
@@ -0,0 +1,23 @@
+  
+
+//    exercise blog post object 
+
+
+    const blogPost = {
+        title : 'job hiring',
+        body :  'need a front end developer with 1 year of experience',
+        author : 'ali',
+        views : 430,
+        Comment : [ 
+            
+                {author : 'a' , body :'b'},
+                {author : 'a' , body :'b'},
+            
+        ],
+         isLive : true
+    }
+
+console.log (blogPost);
+    
+
+
