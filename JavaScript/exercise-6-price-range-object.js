2fa30f129e4237ca498b929181f5fb44c9f43f92
Usman Javed <usmanjaved0816@gmail.com>
2024-10-02 14:13:33 +0500

    solve exercise 6

---
+++ JavaScript/exercise-6-price-range-object.js
@@ -0,0 +1,11 @@
+//   price range 
+
+let priceRange = [
+    { label : '$' , tooltip : 'inexpensive' , minPerPerson: 0 , maxPerPerson: 10},
+    { label : '$$' , tooltip : 'moderate' , minPerPerson: 11 , maxPerPerson: 20},
+    { label : '$$$' , tooltip : 'expensive' , minPerPerson: 21 , maxPerPerson: 50},
+];
+
+let resturents  = [
+    {averagePerPerson : 5}
+]
